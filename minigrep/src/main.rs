89da02e4788fb34310e6f06f913ec5b72aaef81a
James <53650971+jmstudyacc@users.noreply.github.com>
2021-07-15 20:30:17 +0100

    adding better error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,5 @@
 // bring module into scope as code uses the args() function - may panic due to invalid Unicode
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     /* args() function is nested two modules deep std::env::args, as a result convention states
@@ -9,7 +9,14 @@ fn main() {
     let args: Vec<String> = env::args().collect(); // collect() turns iterator into a vector - collect() requires type annotation
 
     // config now creates a new instance of the Config struct
-    let config = Config::new(&args);
+    // main() needs to handle the Result value return
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // unwrap_or_else() requires you to define custom, non-panic! error handling
+        // if the return is Ok() it acts like unwrap() otherwise it calls the code in the closure
+        // an anonymous function defined and passed as an argument to unwrap_or_else()
+        println!("Problem passing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for '{}'", config.query);
     println!("In file {}", config.filename);
@@ -29,10 +36,16 @@ struct Config {
 
 impl Config {
     // creating a method via impl against Config is more idiomatic as we are returning Config struct
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        // include a conditional check to catch basic input error
+        if args.len() < 3 {
+            // instead of calling panic! the function now returns an Err variant
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        // returns an Ok() variant if not an error
+        Ok(Config { query, filename })
     }
 }
