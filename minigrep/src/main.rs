abc3f331ae18ed839ad665efe64375c10153a475
James <53650971+jmstudyacc@users.noreply.github.com>
2021-07-18 19:18:06 +0100

    errors print to stderr not stdout, eprintln!

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -18,8 +18,10 @@ fn main() {
         /* unwrap_or_else() requires you to define custom, non-panic! error handling
          if the return is Ok() it acts like unwrap() otherwise it calls the code in the closure
          an anonymous function defined and passed as an argument to unwrap_or_else()
+
+         eprintln! macro prints errors to stderr and not stdout, provided by the standard library.
         */
-        println!("Problem passing arguments: {}", err);
+        eprintln!("Problem passing arguments: {}", err);
         process::exit(1);
     });
 
@@ -29,9 +31,13 @@ fn main() {
     /* if run returns an error we return the error value and exit the code
     we return () if no error so we do not need to unwrap anything - we only care about errors*/
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
         // print the error and exit
     }
 }
+
+// cargo run to poem.txt > output.txt
+// this will print any errors to stderr - you'll see them on the command line
+// successful runs of this will send the output to output.txt
